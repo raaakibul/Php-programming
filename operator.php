813956cd36bf66e2c81846d42ebec5aeb90d8c4b
rakibul-islam <raakibul.uap@gmail.com>
2023-08-23 22:17:27 +0600

    operator file

---
+++ operator.php
@@ -0,0 +1,20 @@
+<?php
+
+$number1 = 20;
+$number2 = 10;
+
+$addition = $number1 + $number2;
+$subtraction = $number1 - $number2;
+$multiplication = $number1 * $number2;
+$division = $number1 / $number2;
+
+echo "Addition is:", ($addition);
+echo "\nSubtraction is:", ($subtraction);
+echo "\nMultiplication is:", ($multiplication);
+echo "\nDivision is:", ($division);
+
+
+
+
+
+?>
