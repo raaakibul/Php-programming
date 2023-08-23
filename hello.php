b5558f9fbe53765f7d14f99be0d7662d022042db
rakibul-islam <raakibul.uap@gmail.com>
2023-08-23 22:05:34 +0600

    first commit

---
+++ hello.php
@@ -0,0 +1,17 @@
+<?php
+    echo "Hello World\n";
+    echo "This is a test page\n";
+    print("Hello World\n");
+    $number1 = 10;
+    $number2 = 10;
+    echo ($number1 + $number2);
+
+    $name = "Rakibul Islam";
+    echo "\nname: ", $name;
+    echo "\n";
+    echo "Word count is:", strlen("Rakibul Islam");
+
+    $number = 5;
+    var_dump($number);
+
+?>
